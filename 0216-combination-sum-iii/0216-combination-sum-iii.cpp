6ade79131d3f06765135ad090b04e7035438d71b
Anurag_Chaturvedi_49_56 <adityagkumar56@gmail.com>
2025-10-02 17:51:20 +0530

    Time: 0 ms (100%), Space: 9 MB (28.54%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void give ( int num , int k , int n ,vector<int>&temp, vector<vector<int>>&ans)
+    {
+        if ( k <= 0 && n == 0)
+        {
+            ans.push_back(temp);
+            return ;
+        }
+         if (k == 0 || n <= 0 || num > 9) return;
+        temp.push_back(num);
+        give(num+1 , k-1 , n -  num, temp , ans );
+
+        temp.pop_back();
+
+        give(num + 1 , k , n , temp , ans);
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        give(1 , k , n  ,temp, ans);
+
+        return ans;
+    }
+};
